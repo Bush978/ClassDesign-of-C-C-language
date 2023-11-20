a9c6152ee86da837d57d7db06f2ab241b8fe9ca3
Qinglin he <130721325+Bush978@users.noreply.github.com>
2023-11-20 15:01:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-﻿#include <iostream>
 #include "book.h"
-#include <string.h>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -10,18 +7,18 @@ int main()
     int key;
     string name;
     int place;
-    printf("---Books management stystem---\n");
-    printf("1.scan&show the info of the book\n");
-    printf("2.addition the number of the book\n");
-    printf("3.insert the information into the certain place\n");
-    printf("4.delete the information of the book in the certain place\n");
-    printf("5.search the book by place\n");
-    printf("6.search the book by name\n");
-    printf("7.update&grow the prices of the books \n");
-    printf("8.sort the books in ascend order\n");
-    printf("9.sort the books in inverted sequence\n");
-    printf("10.exit\n\n");
-    printf("\nplease choose the option(1-10): ");
+    printf("===== Books Management System =====\n");
+    printf("1. Scan & Show Book Information\n");
+    printf("2. Add the Number of Books\n");
+    printf("3. Insert Information at a Specific Place\n");
+    printf("4. Delete Book Information at a Specific Place\n");
+    printf("5. Search for a Book by Place\n");
+    printf("6. Search for a Book by Name\n");
+    printf("7. Update & Increase Book Prices\n");
+    printf("8. Sort Books in Ascending Order\n");
+    printf("9. Sort Books in Descending Order\n");
+    printf("10. Exit\n\n");
+    printf("Please choose an option (1-10): ");
     cin >> key;
     do {
         switch (key)
@@ -34,28 +31,28 @@ int main()
             printf("\nplease choose the next option: ");
             cin >> key;
             break;
-        case 3:cout << "please input the place: \n";
+        case 3:cout << "please input the position: \n";
             cin >> place;
             l.insertion(place);
             printf("\nplease choose the next option: ");
             cin >> key;
             break;
 
-        case 4:cout << "please input the place: \n";
+        case 4:cout << "please input the position: \n";
             cin >> place;
             l.deletion(place);
             printf("\nplease choose the next option: ");
             cin >> key;
             break;
 
-        case 5:cout << "please input the place: \n";
+        case 5:cout << "please input the position: \n";
             cin >> place;
             l.search(place);
             printf("\nplease choose the next option: ");
             cin >> key;
             break;
 
-        case 6: cout << "please input the name: \n";
+        case 6: cout << "please input the Title: \n";
             cin>> name;
             l.search(name);
             printf("\nplease choose the next option: ");
